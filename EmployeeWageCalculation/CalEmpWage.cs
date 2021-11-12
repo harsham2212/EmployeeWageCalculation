a303a0006787d8a7ea68a96a2901fafb13a8089f
Harsh Singh <kmrharsh50@gmail.com>
2021-11-12 15:34:15 +0530

    [Harsh]Added:Checking Attendance EmployeeWageProble Features Implemented

---
+++ EmployeeWageCalculation/CalEmpWage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCalculation
+{
+    public class CalEmpWage
+    {
+        const int PRESENT = 1;
+        public void EmployeeCheck()
+        {
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ EmployeeWageCalculation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Welcome to EmployeeWage Computation Program on Master Branch");
+            CalEmpWage attendance = new CalEmpWage();
+            attendance.EmployeeCheck();
+        }
+    }
+}
