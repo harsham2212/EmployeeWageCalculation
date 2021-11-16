836abb049e3fad0045f13b4b466afcd6b2daed9b
Harsh Singh <kmrharsh50@gmail.com>
2021-11-16 14:44:25 +0530

    [Harsh]Added:Refactor code EmployeeWageCalculation Features Implemented

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -6,19 +6,9 @@ namespace EmployeeWageCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Displaying Welcome to EmployeeWage Computation Program on Master Branch");
+            Console.WriteLine("Displaying Welcome to EmployeeWage Computation Program");
             CalEmpWage Attendance = new CalEmpWage();
             Attendance.EmployeeCheck();
-            CalEmpWage Wages = new CalEmpWage();
-            Wages.EmpWage();
-            CalEmpWage parttime = new CalEmpWage();
-            parttime.EmployeeDailyWage();
-            CalEmpWage Dailywage = new CalEmpWage();
-            Dailywage.DailyEmployeeWage();
-            CalEmpWage Monthly = new CalEmpWage();
-            Monthly.MonthlyEmpWage();
-            CalEmpWage Hours = new CalEmpWage();
-            Hours.MonthlyEmployeeWage();
         }
     }
 }
